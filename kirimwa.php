cb5fe49e81e33054e11f6a5f5edd1ee53228c791
ndzralatas <hasannadziralatas@gmail.com>
2021-07-04 20:07:35 +0700

    coba 2

--- kirimwa.php
+++ kirimwa.php
@@ -1,9 +1,9 @@
 <?php 
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $pilih = $_POST['pilih'];
-    $message = $_POST['message'];
+if (isset($_POST["submit"])) {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $pilih = $_POST["pilih"];
+    $message = $_POST["message"];
     $nowa = $_POST['nowa'];
     header("location:https://api.whatsapp.com/send?phone=$nowa&text=Hallo My Name is : %20$name%20%0DMy Email : %20$email%20%0DI Want To : %20$pilih%20%0DMy Message is : %20$message");
 }else {
