24319bcb3eb8167268127211d53f2f175fc69e1f
ndzralatas <hasannadziralatas@gmail.com>
2021-07-04 19:51:41 +0700

    oke

--- kirimwa.php
+++ kirimwa.php
@@ -5,7 +5,7 @@ if (isset($_POST['submit'])) {
     $pilih = $_POST['pilih'];
     $message = $_POST['message'];
     $nowa = $_POST['nowa'];
-    header("location:https://api.whatsapp.com/send?phone=$nowa&text=Hallo My Name is : %20$name%20%0DMy Email : %20$email%20%0DI Want To : %20$pilih%20%0DMy Message : %20$message");
+    header("location:https://api.whatsapp.com/send?phone=$nowa&text=Hallo My Name is : %20$name%20%0DMy Email : %20$email%20%0DI Want To : %20$pilih%20%0DMy Message is : %20$message");
 }else {
     echo "<script>window.location=history.go(-1);</script>";
 }
