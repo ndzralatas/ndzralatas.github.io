ed1294a648a9108c82086d66dca52b593901e1e9
ndzralatas <hasannadziralatas@gmail.com>
2021-07-05 11:46:33 +0700

    test post

--- kirimwa.php
+++ kirimwa.php
@@ -5,6 +5,6 @@ if (isset($_POST['submit'])) {
     $pilih = $_POST['pilih'];
     $message = $_POST['message'];
     $nowa = $_POST['nowa'];
-    header("location:https://api.whatsapp.com/send?phone=$nowa&text=Hallo My Name is : %20$name%20%0DMy Email : %20$email%20%0DI Want To : %20$pilih%20%0DMy Message is : %20$message");
+    
+echo("pesan berhasil");    
 }
-?>
